26147ab015fe4c9075a17c8eae43370e1fb53179
RiceAndMeet <jun.ying.yan@ryerson.ca>
2017-02-17 17:51:24 -0500

    Mobile Phone version of CSS added

--- WeatherApp/weather.js
+++ WeatherApp/weather.js
@@ -23,7 +23,6 @@ document.getElementById("F").addEventListener("click",function(){
 
 function currentTemp(){
 	var request = new XMLHttpRequest();
-	
 	request.onreadystatechange=function (){
 		if (request.readyState == 4 && request.status == 200){
 			var data= request.response;
